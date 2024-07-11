8dac1bfc45db00f12ee45151ea16b542254f83fd
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2024-07-11 18:53:04 -0300

    feat: add footer and header section

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -4,38 +4,85 @@ import Image from "apps/website/components/Image.tsx";
 interface Props {
   href?: string;
   image?: ImageWidget;
+  imageDesktop?: ImageWidget;
   alt?: string;
   width?: number;
+  widthDesktop?: number;
   height?: number;
+  heightDesktop?: number;
   text?: string;
+  textHref?: string;
+  subText?: string;
+  socials?: {
+    icon?: ImageWidget;
+    alt?: string;
+    height?: string;
+    width?: string;
+    link?: string;
+  }[]
 }
 
 function Footer({
-  image =
-    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
-  href = "https://deco.cx/",
-  text = "Made with",
-  alt = "Made with deco.cx",
-  height = 20,
-  width = 50,
+  image = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10964/0f72b3b8-86e1-4772-b8a3-7ad57fbb99cd",
+  imageDesktop = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10964/0f72b3b8-86e1-4772-b8a3-7ad57fbb99cd",
+  href = "/",
+  text = "contato@inoa.com.br",
+  textHref = "mailto:contato@inoa.com.br",
+  subText = "Jardim Botânico - Rio de Janeiro - BR",
+  alt = "Inoa Logo",
+  height = 29,
+  heightDesktop = 70,
+  width = 59,
+  widthDesktop = 140,
+  socials,
 }: Props) {
   return (
-    <div class="py-8 lg:px-0 px-6 fixed bottom-0 w-full mx-auto">
-      <a
-        href={href}
-        class="flex flex-row gap-1 items-center justify-center text-xs"
-        target="_blank"
-      >
-        {text && <p>{text}</p>}
-        {image && (
-          <Image
-            src={image || ""}
-            alt={alt || ""}
-            height={height || 20}
-            width={width || 50}
-          />
-        )}
-      </a>
+    <div class="bg-primary ">
+      <div class="max-w-[970px] mx-auto flex justify-between py-[20px] px-[15px] min-h-[140px]">
+        <a
+          href={href}
+          class=""
+          target="_blank"
+        >
+          {image && (
+            <Image
+              class="md:hidden"
+              src={image || ""}
+              alt={alt || ""}
+              height={height || 29}
+              width={width || 59}
+            />
+          )}
+          {imageDesktop && (
+            <Image
+              class="hidden md:block"
+              src={imageDesktop || ""}
+              alt={alt || ""}
+              height={heightDesktop || 70}
+              width={widthDesktop || 140}
+            />
+          )}
+        </a>
+        <div class="flex flex-col items-end">
+          <div class="flex mb-2">
+            {socials &&
+              socials.map((social) => (
+                <Image
+                  src={social.icon || ""}
+                  alt={social.alt || ""}
+                  height={social.height || 30}
+                  width={social.width || 30}
+                  class="ml-[10px]"
+                />
+              ))
+            }
+          </div>
+          <address class="text-right">
+            {text && <a href={textHref}><p class="text-base-300 text-xs italic">{text}</p></a>}
+            {subText && <p class="text-base-300 text-sm italic">{subText}</p>}
+          </address>
+        </div>
+      </div>
     </div>
   );
 }
